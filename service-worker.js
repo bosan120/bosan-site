9c2753f8787a117e35fcd23d47669c0f8cdd6e0f
bosan120 <mohumohu30025@gmail.com>
2020-02-01 16:08:44 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e2d326dd9f086e5ca8e38756360d0ff.js"
+  "/precache-manifest.84e51aa663ff3894c9b1e86c94060775.js"
 );
 
 self.addEventListener('message', (event) => {
